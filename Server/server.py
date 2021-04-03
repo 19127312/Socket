4c1df13293eb8ed7010d1d71476815d61cb6e632
19127312 <63494302+19127312@users.noreply.github.com>
2021-04-03 15:05:59 +0700

    test

--- Server/server.py
+++ Server/server.py
@@ -1,51 +1,26 @@
 import socket
 import sqlite3
 from sys import getsizeof
-
-def checkExist(user):
-    accfile = open("account.txt", "r")
-    for line in accfile.readlines():
-        values = line.split(" ")
-        if user == values[0]:
-            accfile.close()
-            return 1
-    accfile.close()
-    return 0
-
-def addAccount(user,password):
-    accfile = open("account.txt", "a")
-    newAcc="\n"+user+" "+password
-    accfile.write(newAcc)
-    accfile.close()
-
-def CheckAccount(user,password):
-
-    accfile=open( "account.txt" , "r")
-    for line in accfile.readlines():
-        values=line.split(" ")
-        if values[1][len(values[1])-1:len(values[1])]=="\n":
-            values[1]=values[1][0:len(values[1])-1]
-        if user==values[0]:
-            if password==values[1]:
-                accfile.close()
-                return 1
-            else:
-                accfile.close()
-                return 0
-    accfile.close()
-    return -1
+import os
+from _thread import *
 
 HOST = '127.0.0.1'
 PORT = 8000
 
+ThreadCount = 0
 def main () :
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT)) # lắng nghe
-    s.listen(1)
+    s.listen(3)
     conn, addr = s.accept()
+    print('Connected to: ' + addr[0] + ':' + str(addr[1]))
+    start_new_thread(main, (conn,))
+    ThreadCount += 1
     with conn:
         try:
             print('Connected by', addr)
+
             while True:
                 data = conn.recv(1024)
                 data = data.decode('utf-8')
@@ -157,6 +132,38 @@ def main () :
 
         finally:
             s.close()
+def checkExist(user):
+    accfile = open("account.txt", "r")
+    for line in accfile.readlines():
+        values = line.split(" ")
+        if user == values[0]:
+            accfile.close()
+            return 1
+    accfile.close()
+    return 0
+
+def addAccount(user,password):
+    accfile = open("account.txt", "a")
+    newAcc="\n"+user+" "+password
+    accfile.write(newAcc)
+    accfile.close()
+
+def CheckAccount(user,password):
+
+    accfile=open( "account.txt" , "r")
+    for line in accfile.readlines():
+        values=line.split(" ")
+        if values[1][len(values[1])-1:len(values[1])]=="\n":
+            values[1]=values[1][0:len(values[1])-1]
+        if user==values[0]:
+            if password==values[1]:
+                accfile.close()
+                return 1
+            else:
+                accfile.close()
+                return 0
+    accfile.close()
+    return -1
 
 if __name__ == '__main__':
     main()
